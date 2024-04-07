e15f5988f863df2a0a7d64424ff4679fda0b2ef3
wcraske <90268266+wcraske@users.noreply.github.com>
2024-04-06 17:22:08 -0700

    added bottom up implementation

--- visualize.py
+++ visualize.py
@@ -1,7 +1,12 @@
 import tkinter as tk
 import random
 
+objects = []
+knapsack_space = 50
+COLORS = ["red", "orange", "yellow", "blue", "green", "purple", "gold", "magenta", "pink", "cyan"]
+
 def add_random_object():
+    global objects
     print("Add Random Object")
     add_random_object_button.config(relief=tk.SUNKEN)
     run_bottom_up_button.config(relief=tk.RAISED)
@@ -14,13 +19,9 @@ def add_random_object():
     knapsack_height = 300
     knapsack_x = 50
     knapsack_y = 50
-    knapsack_capacity = 50
 
     canvas.create_rectangle(knapsack_x, knapsack_y, knapsack_x + knapsack_width, knapsack_y + knapsack_height, outline="black")
-    canvas.create_text(knapsack_x + knapsack_width/2, knapsack_y - 20, text=f"Capacity: {knapsack_capacity}", tags="labels")
-
-    objects = [] 
-    COLORS = ["red", "orange", "yellow", "blue", "green", "purple", "gold", "magenta", "pink", "cyan"]
+    canvas.create_text(knapsack_x + knapsack_width/2, knapsack_y - 20, text=f"Capacity: {knapsack_space}", tags="labels")
 
     for i in range(10):
         while True:
@@ -40,14 +41,36 @@ def add_random_object():
                 color = COLORS.pop()
                 canvas.create_rectangle(x, y, x + size, y + size, fill=color, tags="objects")
                 canvas.create_text(x + size/2, y + size/2, text=f"Weight: {weight}\nPrice: {price}", tags="labels")
-                objects.append((x, y, size, size))
+                objects.append((x, y, size, size, weight, price))
                 break
 
 def run_bottom_up():
+    global objects, knapsack_space
     print("Run Bottom Up")
     add_random_object_button.config(relief=tk.RAISED)
     run_bottom_up_button.config(relief=tk.SUNKEN)
     run_memFun_button.config(relief=tk.RAISED)
+
+    numObjects = len(objects)
+    dyPro = [[0] * (knapsack_space + 1) for _ in range(numObjects + 1)]
+
+    for i in range(1, numObjects + 1):
+        for j in range(knapsack_space + 1):
+            weight = objects[i - 1][4]
+            price = objects[i - 1][5]
+            if weight <= j:
+                dyPro[i][j] = max(dyPro[i - 1][j], dyPro[i - 1][j - weight] + price)
+            else:
+                dyPro[i][j] = dyPro[i - 1][j]
+
+    knapsack_objects = []
+    j = knapsack_space
+    for i in range(numObjects, 0, -1):
+        if dyPro[i][j] != dyPro[i - 1][j]:
+            knapsack_objects.append(objects[i - 1])
+            j -= objects[i - 1][4]
+
+    print("Selected objects:", knapsack_objects)
 
 def run_memFun():
     print("Run memFun")
